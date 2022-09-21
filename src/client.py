739c8ae5cbd5eca7e3e3ff8ab48aa45cc97b7f59
SgtMarmite <d.novotny31415@gmail.com>
2022-09-21 15:03:43 +0200

    first release

---
+++ src/client.py
@@ -0,0 +1,95 @@
+import requests
+import json
+import os
+from pathlib import Path
+import logging
+
+from requests.exceptions import HTTPError
+from keboola.component.exceptions import UserException
+
+
+ARTIFACTS = ["manifest.json", "catalog.json", "run_results.json"]
+
+
+def store_artifact(artifact: str, file: dict) -> None:
+    cwd = Path(os.getcwd())
+    root_dir = cwd.parent.absolute()
+    artifacts_dir = (os.path.join(root_dir, "data", "out", "artifacts"))
+
+    if not os.path.exists(artifacts_dir):
+        logging.info("Creating artifacts directory.")
+        os.makedirs(artifacts_dir)
+
+    full_path = os.path.join(artifacts_dir, artifact)
+
+    with open(full_path, "w") as f:
+        json.dump(file, f)
+
+
+class DbtClient:
+
+    def __init__(self, account_id,
+                 job_id,
+                 api_key
+                 ):
+        self.account_id = account_id
+        self.job_id = job_id
+        self.api_key = api_key
+
+        self.auth_headers = {'Authorization': f"Token {api_key}"}
+
+    def get_artifacts(self, job_run_id: int) -> None:
+        """
+        Gets available artifacts and stores them in artifacts folder.
+        Args:
+            job_run_id: Job run ID
+        """
+        for artifact in ARTIFACTS:
+            res = requests.get(
+                url=f"https://cloud.getdbt.com/api/v2/accounts/{self.account_id}/runs/{job_run_id}/artifacts/{artifact}",
+                headers=self.auth_headers
+            )
+            if res.status_code == 200:
+                store_artifact(artifact, res.json())
+            else:
+                logging.warning(f"Cannot save {artifact}: {res.text}")
+
+    def trigger_job(self, cause: str) -> int:
+        """
+        Triggers
+        Args:
+            cause: String identifier which will be sent along with job trigger request.
+        """
+        res = requests.post(
+            url=f"https://cloud.getdbt.com/api/v2/accounts/{self.account_id}/jobs/{self.job_id}/run/",
+            headers=self.auth_headers,
+            json={
+                # Optionally pass a description that can be viewed within the dbt Cloud API.
+                # See the API docs for additional parameters that can be passed in,
+                # including `schema_override`
+                'cause': cause,
+            }
+        )
+
+        try:
+            res.raise_for_status()
+        except HTTPError:
+            raise UserException(f"Encountered Error when triggering job: {res.text}")
+
+        response_payload = res.json()
+        return response_payload['data']['id']
+
+    def get_job_run_status(self, job_run_id: int) -> int:
+        """
+        Fetches Job run status code.
+        Args:
+            job_run_id: Job run ID
+        """
+        res = requests.get(
+            url=f"https://cloud.getdbt.com/api/v2/accounts/{self.account_id}/runs/{job_run_id}/",
+            headers=self.auth_headers,
+        )
+
+        res.raise_for_status()
+        response_payload = res.json()
+        return response_payload['data']['status']
---
+++ src/component.py
@@ -0,0 +1,117 @@
+"""
+Template Component main class.
+inspired by:
+https://discourse.getdbt.com/t/triggering-a-dbt-cloud-job-in-your-automated-workflow-with-python/2573
+
+
+"""
+import csv
+import logging
+from datetime import datetime
+import time
+
+from keboola.component.base import ComponentBase
+from helper import DbtJobRunStatus
+from client import DbtClient
+
+from keboola.component.exceptions import UserException
+
+# configuration variables
+ACCOUNT_ID = "account_id"
+JOB_ID = "job_id"
+API_KEY = "#api_key"
+CAUSE = "cause"
+WAIT_FOR_RESULT = "wait_for_result"
+MAX_WAIT_TIME = "max_wait_time"
+
+# list of mandatory parameters => if some is missing,
+# component will fail with readable message on initialization.
+REQUIRED_PARAMETERS = [ACCOUNT_ID, JOB_ID, API_KEY, CAUSE, WAIT_FOR_RESULT]
+REQUIRED_IMAGE_PARS = []
+
+
+class Component(ComponentBase):
+    """
+        Extends base class for general Python components. Initializes the CommonInterface
+        and performs configuration validation.
+
+        For easier debugging the data folder is picked up by default from `../data` path,
+        relative to working directory.
+
+        If `debug` parameter is present in the `config.json`, the default logger is set to verbose DEBUG mode.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.validate_configuration_parameters(REQUIRED_PARAMETERS)
+        params = self.configuration.parameters
+
+        self.account_id = params.get(ACCOUNT_ID)
+        self.job_id = params.get(JOB_ID)
+        self.api_key = params.get(API_KEY)
+        self.cause = params.get(CAUSE)
+        if wait_for_result := params.get(WAIT_FOR_RESULT) is not None:
+            self.max_wait_time = params.get(MAX_WAIT_TIME * 60)
+        self.wait_for_result = wait_for_result
+
+    def run(self):
+        """
+        Main execution code
+        """
+
+        client = DbtClient(account_id=self.account_id,
+                           job_id=self.job_id,
+                           api_key=self.api_key
+                           )
+
+        job_run_id = client.trigger_job(cause=self.cause)
+        logging.info(f"Triggered Job Run ID: {job_run_id}")
+
+        if self.wait_for_result:
+            start_time = time.time()
+            while True:
+                time.sleep(5)
+
+                status = client.get_job_run_status(job_run_id)
+                logging.info(f"Job status = {DbtJobRunStatus(status).name}")
+
+                if status == DbtJobRunStatus.SUCCESS:
+                    client.get_artifacts(job_run_id)
+                    break
+                elif status == DbtJobRunStatus.ERROR or status == DbtJobRunStatus.CANCELLED:
+                    raise UserException(f"Job with ID {job_run_id} has been stopped.")
+
+                # Stop waiting if runtime is greater than MAX_WAIT_TIME (minutes)
+                if self.max_wait_time:
+                    if (time.time() - start_time) < self.max_wait_time:
+                        raise UserException(f"Max wait time reached for Job with ID {job_run_id} - Exiting.")
+
+        table = self.create_out_table_definition("output.csv", incremental=True, primary_key=["job_run_id", "ts"])
+
+        # DO whatever and save into out_table_path
+        with open(table.full_path, mode="wt", encoding="utf-8", newline="") as out_file:
+            writer = csv.DictWriter(out_file, fieldnames=["job_run_id", "ts"])
+            writer.writeheader()
+            writer.writerow({
+                "job_run_id": job_run_id,
+                "ts": datetime.now().isoformat()
+            })
+
+        self.write_manifest(table)
+
+
+"""
+        Main entrypoint
+"""
+if __name__ == "__main__":
+    try:
+        comp = Component()
+        # this triggers the run method by default and is controlled by the configuration.action parameter
+        comp.execute_action()
+    except UserException as exc:
+        logging.exception(exc)
+        exit(1)
+    except Exception as exc:
+        logging.exception(exc)
+        exit(2)
